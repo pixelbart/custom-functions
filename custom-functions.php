a4b6c8dfc640060c7e2f89afb3b4a855204d684e
Kevin Pliester <me@pixelbart.de>
2017-01-31 00:11:53 +0100

    Update custom-functions.php

--- custom-functions.php
+++ custom-functions.php
@@ -15,6 +15,6 @@ Author URI: #
 function load_my_custom_scripts_31012017()
 {
     wp_enqueue_script( 'my_custom_js', plugins_url( 'custom.js', __FILE__ ), array('jquery') );
-    wp_enqueue_style ( 'my_custom_css',    plugins_url( 'custom.css',    __FILE__ ), false );
+    wp_enqueue_style ( 'my_custom_css', plugins_url( 'custom.css', __FILE__ ), false );
 }
 add_action('wp_enqueue_scripts', 'load_my_custom_scripts_31012017');
